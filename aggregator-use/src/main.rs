f43e57651c5709976af42c07d6475f29e8a2f1f6
Joshy Orndorff <admin@joshyorndorff.com>
2023-03-01 14:07:18 -0500

    remove commented code

--- aggregator-use/src/main.rs
+++ aggregator-use/src/main.rs
@@ -16,19 +16,6 @@ enum Vehicle {
     Car(Car),
 }
 
-// The following impl blocks should be written by the macro
-// impl From<Bike> for Vehicle {
-//     fn from(b: Bike) -> Self {
-//         Self::Bike(b)
-//     }
-// }
-
-// impl From<Car> for Vehicle {
-//     fn from(c: Car) -> Self {
-//         Self::Car(c)
-//     }
-// }
-
 fn main() {
     let balance_bike = Bike { gears: 0 };
     let v_bike: Vehicle = balance_bike.into();
